0846be61e01c2151139b121682e60c9e980055ac
Brandon <BrandonPilane@gmail.com>
2025-05-08 01:25:01 +0200

    Handle updating of tasks

--- taskr/cli.py
+++ taskr/cli.py
@@ -37,3 +37,17 @@ def list():
     for i, task in enumerate(tasks, 1):
         status = "✓" if task["done"] else "✕"
         click.echo(f"{i}. {task['title']} [ {status} ]")
+
+@cli.command()
+@click.argument("id")
+@click.argument("title")
+def update(id, title):
+    tasks = load_tasks()
+    for i, task in enumerate(tasks):
+        if task["id"] == int(id):
+            changed_title = tasks[i]["title"]
+            tasks[i]["title"] = title
+            save_tasks(tasks)
+            click.echo(f"Updated task {id}:\n{changed_title}" + click.style(" → ", fg="green") + f"{title}")
+            return
+    click.echo(f"Task with id {id} not found")
